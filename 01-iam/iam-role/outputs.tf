5661c6f6522a3816bddc6e7efcd4e03d4cae4c67
Iraj Norouzi <useriraj2000@gmail.com>
2022-08-30 17:48:34 +0430

    added iam role

--- 01-iam/iam-role/outputs.tf
+++ 01-iam/iam-role/outputs.tf
@@ -89,31 +89,3 @@ output "aws_key_pair" {
 
 
 
-
-
-
-# output "iam_user_name" {
-#   description = "The user's name"
-#   value       = module.iam_user.iam_user_name
-# }
-
-# output "iam_user_password" {
-#   description = "Password of the created user"
-#   value       = module.iam_user.iam_user_password
-# }
-
-# output "iam_user_accresskeyid" {
-#   description = "AccessKeyId"
-#   value       = module.iam_user.iam_user_accesskey
-# }
-
-# output "iam_user_secret" {
-#   description = "Secret"
-#   value       = module.iam_user.iam_user_secret
-#   sensitive   = true
-# }
-
-# output "iam_group_name" {
-#   description = "The user's name"
-#   value       = module.iam_user.iam_group_name
-# }
--- modules/01-iam/iam-role/outputs.tf
+++ modules/01-iam/iam-role/outputs.tf
@@ -72,27 +72,3 @@ output "aws_key_pair" {
   value = aws_key_pair.deployer
   
 }
-
-
-
-
-# output "iam_user_name" {
-#   value = one(aws_iam_user.dvhb[*].name)
-# }
-
-# output "iam_user_password" {
-#   value = one(aws_iam_user_login_profile.dvhb[*].password)
-# }
-
-# output "iam_user_accesskey" {
-#   value = one(aws_iam_access_key.dvhb[*].id)
-# }
-
-# output "iam_user_secret" {
-#   value     = one(aws_iam_access_key.dvhb[*].secret)
-#   sensitive = true
-# }
-
-# output "iam_group_name" {
-#     value = one(aws_iam_group.dvhb[*].name)
-# }
