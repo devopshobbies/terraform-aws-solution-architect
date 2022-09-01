b4a6358466a89c4f6eb745404561e1f6cc686d72
Iraj Norouzi <useriraj2000@gmail.com>
2022-09-01 11:43:55 +0430

    changed flavor variable to instance_type

--- modules/01-iam/iam-role/main.tf
+++ modules/01-iam/iam-role/main.tf
@@ -17,7 +17,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 }
 resource "aws_instance" "web" {
   ami           = var.ami
-  instance_type =  var.flavor
+  instance_type =  var.instance_type
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   vpc_security_group_ids = [aws_security_group.main.id]
   key_name= "aws_key"
