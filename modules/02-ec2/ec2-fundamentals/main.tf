c8359ccadb7bffa95a5497f26bf099667011d84c
Iraj Norouzi <useriraj2000@gmail.com>
2022-09-10 12:52:57 +0430

    aws_ami fillter

--- modules/02-ec2/ec2-fundamentals/main.tf
+++ modules/02-ec2/ec2-fundamentals/main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "amazon-linux-2" {
   }
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
   }
 }
 resource "aws_instance" "web" {
@@ -27,26 +27,44 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "main" {
   name        = "Main Security Group"
   description = "Main Security Group"
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+    ingress {
+    description      = "ICMP from VPC"
+    from_port        = -1
+    to_port          = -1
+    protocol         = "icmp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "MAIN Security Group"
   }
-}
-// SSH can only be accessed from the WWW network (0.0.0.0/0)
-resource "aws_security_group_rule" "ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.main.id
-}
-resource "aws_security_group_rule" "http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.main.id
 }
 resource "aws_key_pair" "deployer" {
   key_name   = "aws_key"
