c7268a537ebda38eda4b1f29ff713fe6e1c4687f
Iraj Norouzi <useriraj2000@gmail.com>
2022-09-10 13:02:47 +0430

    terraform fmt

--- modules/02-ec2/ec2-fundamentals/main.tf
+++ modules/02-ec2/ec2-fundamentals/main.tf
@@ -10,57 +10,57 @@ data "aws_ami" "amazon-linux-2" {
   }
 }
 resource "aws_instance" "web" {
-  for_each = var.ec2_deployments
-  ami = data.aws_ami.amazon-linux-2.id 
-  instance_type = each.value.instance_type
+  for_each               = var.ec2_deployments
+  ami                    = data.aws_ami.amazon-linux-2.id
+  instance_type          = each.value.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-  key_name= "aws_key"
-  user_data = "${file("user-data-httpd.sh")}"
+  key_name               = "aws_key"
+  user_data              = file("user-data-httpd.sh")
   tags = {
     Name = each.key
   }
-    depends_on = [
-      aws_security_group.main
-    ]
+  depends_on = [
+    aws_security_group.main
+  ]
 }
 // Secure the PgSQL RDS cluster using a dedicated SG
 resource "aws_security_group" "main" {
   name        = "Main Security Group"
   description = "Main Security Group"
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
-    description      = "ICMP from VPC"
-    from_port        = -1
-    to_port          = -1
-    protocol         = "icmp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  ingress {
+    description = "ICMP from VPC"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "HTTP from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "MAIN Security Group"
@@ -68,5 +68,7 @@ resource "aws_security_group" "main" {
 }
 resource "aws_key_pair" "deployer" {
   key_name   = "aws_key"
-  public_key =  var.public_key
+  public_key = var.public_key
 }
+
+
--- modules/02-ec2/ec2-fundamentals/outputs.tf
+++ modules/02-ec2/ec2-fundamentals/outputs.tf
@@ -1,36 +1,36 @@
 output "id" {
   description = "The ID of the instance"
-  value       =  {for k,v in aws_instance.web: k => v.id}  
+  value       = { for k, v in aws_instance.web : k => v.id }
 }
 output "private_dns" {
   description = "The private DNS name assigned to the instance. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
-  value       =  {for k,v in aws_instance.web: k => v.private_dns}  
+  value       = { for k, v in aws_instance.web : k => v.private_dns }
 }
 output "public_dns" {
   description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       =  {for k,v in aws_instance.web: k => v.public_dns}  
+  value       = { for k, v in aws_instance.web : k => v.public_dns }
 }
 output "public_ip" {
   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       =  {for k,v in aws_instance.web: k => v.public_ip}  
+  value       = { for k, v in aws_instance.web : k => v.public_ip }
 }
 output "private_ip" {
   description = "The private IP address assigned to the instance."
-  value       =  {for k,v in aws_instance.web: k => v.private_ip}  
+  value       = { for k, v in aws_instance.web : k => v.private_ip }
 }
 output "aws_security_group_name" {
   description = "aws_security_group name"
-  value = aws_security_group.main.name
+  value       = aws_security_group.main.name
 }
 output "aws_security_group_ingress" {
   description = "aws_security_group ingress"
-  value = aws_security_group.main.ingress
+  value       = aws_security_group.main.ingress
 }
 output "aws_security_group_id" {
   description = "aws_security_group id"
-  value = aws_security_group.main.id
+  value       = aws_security_group.main.id
 }
 output "aws_key_pair" {
   description = "aws_key_pair public_key"
-  value = aws_key_pair.deployer
+  value       = aws_key_pair.deployer
 }
